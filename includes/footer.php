d44c5a52324e14b3a511c6f5038451aee11b4e49
ajdinmasnic <60182981+ajdinmasnic@users.noreply.github.com>
2020-01-29 22:14:53 +0100

    Sredjen Header i footer
    Sredjen Header i footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
+
+  </div> 
+  <div id='footer' class="p-3 mb-2 bg-dark text-white fixed-bottom">
+  <p class='text-center'>Copyright &copy; School Management System <?php echo date ('Y'); ?></p>
+  </div>
+
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
--- includes/header.php
+++ includes/header.php
@@ -11,3 +11,23 @@
     <title>School Management - <?php echo $title; ?></title>
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">School Management</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mr-auto">
+      <a class="nav-item nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="about.php">About</a>
+      <a class="nav-item nav-link" href="contact.php">Contact</a>
+      
+    </div>
+    <div class="navbar-nav ml-auto">
+    <a class="nav-item nav-link" href="#">Login <span class="sr-only">(current)</span></a>
+    </div>
+  </div>
+  </nav>
+  <br />
+  <div class='container'>
+  
